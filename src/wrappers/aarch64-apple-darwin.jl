731a87db714207e30b789b9a574c8c698d0fb613
jlbuild <juliabuildbot@gmail.com>
2025-06-22 22:58:11 +0000

    PROJ_jll build 902.600.200+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -5,16 +5,16 @@ using SQLite_jll
 using Libtiff_jll
 using LibCURL_jll
 JLLWrappers.@generate_wrapper_header("PROJ")
-JLLWrappers.@declare_executable_product(cct)
 JLLWrappers.@declare_file_product(ch)
-JLLWrappers.@declare_file_product(gl27)
-JLLWrappers.@declare_file_product(itrf2008)
-JLLWrappers.@declare_file_product(itrf2014)
-JLLWrappers.@declare_library_product(libproj, "@rpath/libproj.25.dylib")
-JLLWrappers.@declare_executable_product(cs2cs)
 JLLWrappers.@declare_executable_product(geod)
 JLLWrappers.@declare_executable_product(gie)
 JLLWrappers.@declare_file_product(itrf2000)
+JLLWrappers.@declare_file_product(itrf2008)
+JLLWrappers.@declare_library_product(libproj, "@rpath/libproj.25.dylib")
+JLLWrappers.@declare_executable_product(cct)
+JLLWrappers.@declare_executable_product(cs2cs)
+JLLWrappers.@declare_file_product(gl27)
+JLLWrappers.@declare_file_product(itrf2014)
 JLLWrappers.@declare_file_product(nad27)
 JLLWrappers.@declare_file_product(nad83)
 JLLWrappers.@declare_file_product(nad_lst)
@@ -27,40 +27,9 @@ JLLWrappers.@declare_executable_product(projsync)
 JLLWrappers.@declare_file_product(world)
 function __init__()
     JLLWrappers.@generate_init_header(SQLite_jll, Libtiff_jll, LibCURL_jll)
-    JLLWrappers.@init_executable_product(
-        cct,
-        "bin/cct",
-    )
-
     JLLWrappers.@init_file_product(
         ch,
         "share/proj/CH",
-    )
-
-    JLLWrappers.@init_file_product(
-        gl27,
-        "share/proj/GL27",
-    )
-
-    JLLWrappers.@init_file_product(
-        itrf2008,
-        "share/proj/ITRF2008",
-    )
-
-    JLLWrappers.@init_file_product(
-        itrf2014,
-        "share/proj/ITRF2014",
-    )
-
-    JLLWrappers.@init_library_product(
-        libproj,
-        "lib/libproj.25.dylib",
-        RTLD_LAZY | RTLD_DEEPBIND,
-    )
-
-    JLLWrappers.@init_executable_product(
-        cs2cs,
-        "bin/cs2cs",
     )
 
     JLLWrappers.@init_executable_product(
@@ -76,6 +45,37 @@ function __init__()
     JLLWrappers.@init_file_product(
         itrf2000,
         "share/proj/ITRF2000",
+    )
+
+    JLLWrappers.@init_file_product(
+        itrf2008,
+        "share/proj/ITRF2008",
+    )
+
+    JLLWrappers.@init_library_product(
+        libproj,
+        "lib/libproj.25.dylib",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        cct,
+        "bin/cct",
+    )
+
+    JLLWrappers.@init_executable_product(
+        cs2cs,
+        "bin/cs2cs",
+    )
+
+    JLLWrappers.@init_file_product(
+        gl27,
+        "share/proj/GL27",
+    )
+
+    JLLWrappers.@init_file_product(
+        itrf2014,
+        "share/proj/ITRF2014",
     )
 
     JLLWrappers.@init_file_product(
